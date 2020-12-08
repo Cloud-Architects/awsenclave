edb67d90cf72b4b12ade28e855ba11c260b21e0e
Patryk Orwat <patrykorwat@gmail.com>
2020-12-09 01:19:06 +0800

    Reusing KMS CMK keys

--- aws-enclave-setup/src/main/java/solutions/cloudarchitects/awsenclave/Main.java
+++ aws-enclave-setup/src/main/java/solutions/cloudarchitects/awsenclave/Main.java
@@ -1,11 +1,11 @@
 package solutions.cloudarchitects.awsenclave;
 
+import com.amazonaws.services.kms.AWSKMS;
+import com.amazonaws.services.kms.AWSKMSClientBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import software.amazon.awssdk.services.ec2.Ec2Client;
 import software.amazon.awssdk.services.ec2.model.TerminateInstancesRequest;
-import software.amazon.awssdk.services.kms.KmsClient;
-import software.amazon.awssdk.services.kms.model.KeyMetadata;
 import solutions.cloudarchitects.awsenclave.model.Ec2Instance;
 import solutions.cloudarchitects.awsenclave.model.EnclaveMeasurements;
 import solutions.cloudarchitects.awsenclave.model.KeyPair;
@@ -15,18 +15,18 @@ class Main {
 
     public static void main(String... args) {
         Ec2Client ec2Client = Ec2Client.create();
-        KmsClient kmsClient = KmsClient.create();
+        AWSKMS kmsClient = AWSKMSClientBuilder.defaultClient();
         ParentAdministratorService parentAdministratorService = new ParentAdministratorService(ec2Client);
         OwnerService ownerService = new OwnerService(kmsClient);
 
         KeyPair keyPair = parentAdministratorService.loadKey();
         Ec2Instance ec2Instance = parentAdministratorService.createParent(keyPair);
         try {
-            KeyMetadata keyMetadata = ownerService.setupCrypto();
             parentAdministratorService.prepareSampleEnclave(keyPair, ec2Instance);
             EnclaveMeasurements enclaveMeasurements = parentAdministratorService.buildEnclave(keyPair, ec2Instance);
             // TODO: update key policy to add measurement attributes
-            byte[] bytes = ownerService.encryptSample(keyMetadata);
+            String keyId = ownerService.setupCrypto();
+            byte[] bytes = ownerService.encryptSample(keyId);
             // TODO: store sample data
             parentAdministratorService.runEnclave(keyPair, ec2Instance);
         } finally {
--- aws-enclave-setup/src/main/java/solutions/cloudarchitects/awsenclave/OwnerService.java
+++ aws-enclave-setup/src/main/java/solutions/cloudarchitects/awsenclave/OwnerService.java
@@ -5,39 +5,52 @@ import com.amazonaws.encryptionsdk.CommitmentPolicy;
 import com.amazonaws.encryptionsdk.CryptoResult;
 import com.amazonaws.encryptionsdk.kms.KmsMasterKey;
 import com.amazonaws.encryptionsdk.kms.KmsMasterKeyProvider;
-import software.amazon.awssdk.services.kms.KmsClient;
-import software.amazon.awssdk.services.kms.model.*;
+import com.amazonaws.services.kms.AWSKMS;
+import com.amazonaws.services.kms.model.AliasListEntry;
+import com.amazonaws.services.kms.model.CreateAliasRequest;
+import com.amazonaws.services.kms.model.CreateKeyRequest;
+import com.amazonaws.services.kms.model.CreateKeyResult;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.Collections;
 import java.util.Map;
+import java.util.Optional;
 
 public class OwnerService {
     private static final String EXAMPLE_DATA = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do " +
             "eiusmod tempor incididunt ut labore et dolore magna aliqua";
 
-    private final KmsClient kmsClient;
+    private final AWSKMS kmsClient;
 
-    public OwnerService(KmsClient kmsClient) {
+    public OwnerService(AWSKMS kmsClient) {
         this.kmsClient = kmsClient;
     }
 
-    public KeyMetadata setupCrypto() {
-        CreateKeyRequest createKeyRequest = CreateKeyRequest.builder()
-                .build();
-        CreateKeyResponse key = kmsClient.createKey(createKeyRequest);
-        return key.keyMetadata();
+    public String setupCrypto() {
+        Optional<String> enclaveKeyId = kmsClient.listAliases().getAliases().stream()
+                .filter(alias -> alias.getAliasName().equals("enclave"))
+                .map(AliasListEntry::getTargetKeyId)
+                .findAny();
+        if (enclaveKeyId.isPresent()) {
+            return enclaveKeyId.get();
+        }
+        CreateKeyRequest createKeyRequest = new CreateKeyRequest();
+        CreateKeyResult key = kmsClient.createKey(createKeyRequest);
+        kmsClient.createAlias(new CreateAliasRequest()
+                .withTargetKeyId(key.getKeyMetadata().getKeyId())
+                .withAliasName("enclave"));
+        return key.getKeyMetadata().getKeyId();
     }
 
-    public byte[] encryptSample(KeyMetadata keyMetadata) {
+    public byte[] encryptSample(String keyId) {
 
         final AwsCrypto crypto = AwsCrypto.builder()
                 .withCommitmentPolicy(CommitmentPolicy.RequireEncryptRequireDecrypt)
                 .build();
 
         final KmsMasterKeyProvider keyProvider = KmsMasterKeyProvider.builder()
-                .buildStrict(keyMetadata.arn());
+                .buildStrict(keyId);
         final Map<String, String> encryptionContext = Collections.singletonMap("enclaveName", "aws-enclave");
         final CryptoResult<byte[], KmsMasterKey> encryptResult = crypto
                 .encryptData(keyProvider, EXAMPLE_DATA.getBytes(StandardCharsets.UTF_8), encryptionContext);
