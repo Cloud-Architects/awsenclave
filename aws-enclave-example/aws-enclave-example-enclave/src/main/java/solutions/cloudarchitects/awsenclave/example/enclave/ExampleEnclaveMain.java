d30a458a8ffc5e3aa4026e96f3194d1c2bba01e7
Patryk Orwat <patrykorwat@gmail.com>
2020-12-13 21:04:11 +0800

    proxy fixing

--- aws-enclave-example/aws-enclave-example-enclave/src/main/java/solutions/cloudarchitects/awsenclave/example/enclave/ExampleEnclaveMain.java
+++ aws-enclave-example/aws-enclave-example-enclave/src/main/java/solutions/cloudarchitects/awsenclave/example/enclave/ExampleEnclaveMain.java
@@ -35,7 +35,7 @@ public class ExampleEnclaveMain {
     private static final Logger LOG = LoggerFactory.getLogger(ExampleEnclaveMain.class);
 
     public static void main(String[] args) throws IOException {
-        LOG.info(execCmd("ifconfig lo 127.0.0.1"));
+        LOG.info(execCmd("/usr/sbin/ip addr add 127.0.0.1 dev lo"));
 
         final String[] proxyExceptionMessage = {"None"};
         InetAddress loopbackAddress = InetAddress.getLoopbackAddress();
