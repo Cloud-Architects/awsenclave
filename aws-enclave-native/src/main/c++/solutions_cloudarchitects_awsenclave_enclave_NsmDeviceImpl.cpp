7a3cf9d55ff1d903279a487e46c6ff47f5017b37
Patryk Orwat <patrykorwat@gmail.com>
2020-12-19 23:34:37 +0800

    implemented ioctl call to NSM device

--- aws-enclave-native/src/main/c++/solutions_cloudarchitects_awsenclave_enclave_NsmDeviceImpl.cpp
+++ aws-enclave-native/src/main/c++/solutions_cloudarchitects_awsenclave_enclave_NsmDeviceImpl.cpp
@@ -13,9 +13,15 @@
 #define NSM_IOCTL_MAGIC 0x0A
 #define NSM_RESPONSE_MAX_SIZE 0x3000
 
+struct nsm_message_t {
+  iovec request;
+  iovec response;
+} ;
+
 JNIEXPORT void JNICALL Java_solutions_cloudarchitects_awsenclave_enclave_NsmDeviceImpl_initialize
   (JNIEnv *env, jobject thisObject) {
     int fd = open("/dev/nsm", O_RDWR);
+
     jclass NsmDeviceImplClass = env->FindClass("solutions/cloudarchitects/awsenclave/enclave/NsmDeviceImpl");
     jfieldID fdField = env->GetFieldID(NsmDeviceImplClass, "fd", "I");
     env->SetIntField(thisObject, fdField, fd);
@@ -25,17 +31,44 @@ JNIEXPORT void JNICALL Java_solutions_cloudarchitects_awsenclave_enclave_NsmDevi
   (JNIEnv *env, jobject thisObject) {
     jclass NsmDeviceImplClass = env->FindClass("solutions/cloudarchitects/awsenclave/enclave/NsmDeviceImpl");
     jfieldID fdField = env->GetFieldID(NsmDeviceImplClass, "fd", "I");
-    int s = (int)env->GetIntField(thisObject, fdField);
+    int fd = (int)env->GetIntField(thisObject, fdField);
 
-    if (s == -1) {
+    if (fd == -1) {
         return;
     }
 
-    int status = close(s);
+    int status = close(fd);
 
     env->SetIntField(thisObject, fdField, -1);
 
     if (status != 0) {
        env->ThrowNew(env->FindClass("java/lang/IllegalArgumentException"), ("Error while closing: " + std::to_string(status)).c_str());
     }
+}
+
+JNIEXPORT jbyteArray JNICALL Java_solutions_cloudarchitects_awsenclave_enclave_NsmDeviceImpl_processRequestInternal
+  (JNIEnv *env, jobject thisObject, jbyteArray request) {
+    jclass NsmDeviceImplClass = env->FindClass("solutions/cloudarchitects/awsenclave/enclave/NsmDeviceImpl");
+    jfieldID fdField = env->GetFieldID(NsmDeviceImplClass, "fd", "I");
+    int fd = (int)env->GetIntField(thisObject, fdField);
+
+    char RESPONSE[NSM_RESPONSE_MAX_SIZE];
+
+    jbyte* request_bytes = env->GetByteArrayElements(request, NULL);
+    jint request_length = env->GetArrayLength((jarray) request);
+
+    nsm_message_t message;
+    message.request.iov_base = (void *) request_bytes;
+    message.request.iov_len = (size_t) request_length;
+    message.response.iov_base = (void *) &RESPONSE;
+    message.response.iov_len = NSM_RESPONSE_MAX_SIZE;
+
+    ioctl(fd, _IOWR(NSM_IOCTL_MAGIC, 0, sizeof(nsm_message_t)), &message);
+
+    env->ReleaseByteArrayElements(request, request_bytes, JNI_ABORT);
+
+    jbyteArray response = env->NewByteArray((jsize) message.response.iov_len);
+    env->SetByteArrayRegion(response, 0, (jsize) message.response.iov_len, (const jbyte*) message.response.iov_base);
+
+    return response;
 }
